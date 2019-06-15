f2f2b0eaaf33108699199c4fa5b386187b5b278b
Jon Atxa <atxajon@gmail.com>
2019-06-15 20:14:10 +0200

    created entity ToDo, added migrations to create db tables, modified AppFixtures.php to load in all ToDos and properties

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,30 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\ToDo;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+      for ($i = 1; $i < 10; $i++) {
+        $this->createToDo($manager, $i);
+      }
     }
+
+  /**
+   *
+   * @param ObjectManager $manager
+   * @param int $n
+   */
+  private function createToDo(ObjectManager $manager, $n)
+  {
+    $todo = new ToDo();
+    $todo->setTitle('ToDo ' . $n);
+    $todo->setBody('Lorem ipsum dolor sit amet, consectetur adipiscing elit...');
+    $todo->setCreatedAt(new \DateTime(date("Y-m-d H:i:s", mt_rand(1262055681, 1562055681))));
+    $todo->setDone(rand(0,1) == 1);
+    $manager->persist($todo);
+    $manager->flush();
+  }
 }
---
+++ src/Entity/ToDo.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ToDoRepository")
+ */
+class ToDo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $body;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $CreatedAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $Done;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(?string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->CreatedAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $CreatedAt): self
+    {
+        $this->CreatedAt = $CreatedAt;
+
+        return $this;
+    }
+
+    public function getDone(): ?bool
+    {
+        return $this->Done;
+    }
+
+    public function setDone(bool $Done): self
+    {
+        $this->Done = $Done;
+
+        return $this;
+    }
+}
